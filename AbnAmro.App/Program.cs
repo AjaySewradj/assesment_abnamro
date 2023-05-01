51cfa0fd61c66ec5690e1664ad8cc717cce4ba97
Ajay Sewradj <ajaysewradj85@gmail.com>
2023-05-01 22:59:52 +0200

    Added simple solution for Implementation 1

---
+++ AbnAmro.App/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AbnAmro.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("\nPlease enter your first name:");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("\nPlease enter your last name:");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine($"\nThanks. You've entered '{firstName} {lastName}'");
+
+            var range = Enumerable.Range(1, 100);
+            foreach (int number in range)
+            {
+                if (number % 3 == 0 && number % 5 == 0) Console.WriteLine($"{firstName} {lastName}");
+                else if (number % 3 == 0) Console.WriteLine(firstName);
+                else if (number % 5 == 0) Console.WriteLine(lastName);
+                else Console.WriteLine(number);
+            }
+        }
+    }
+}
