33803844c6ce372a54885241917566ff14aa7cfb
Ajay Sewradj <ajaysewradj85@gmail.com>
2023-05-02 16:19:18 +0200

    Extracted calculation logic

---
+++ AbnAmroApp/CalculationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbnAmroApp.ConsoleApp
+{
+    public class CalculationService
+    {
+        public IEnumerable<string> Calculate(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentNullException(nameof(firstName));
+            if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentNullException(nameof(lastName));
+
+            var range = Enumerable.Range(1, 100);
+            foreach (int number in range)
+            {
+                if (IsDivisibleBy(number, 3) && IsDivisibleBy(number, 5)) yield return $"{firstName} {lastName}";
+                else if (IsDivisibleBy(number, 3)) yield return firstName;
+                else if (IsDivisibleBy(number, 5)) yield return lastName;
+                else yield return number.ToString();
+            }
+        }
+
+        public bool IsDivisibleBy(int number, int divisor)
+        {
+            return number % divisor == 0;
+        }
+    }
+}
--- AbnAmroApp/Program.cs
+++ AbnAmroApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AbnAmro.App
+﻿using System;
+
+namespace AbnAmroApp.ConsoleApp
 {
     internal class Program
     {
@@ -7,18 +9,18 @@
             Console.WriteLine("Welcome!");
             Console.WriteLine("\nPlease enter your first name:");
             var firstName = Console.ReadLine();
+
             Console.WriteLine("\nPlease enter your last name:");
             var lastName = Console.ReadLine();
 
             Console.WriteLine($"\nThanks. You've entered '{firstName} {lastName}'");
 
-            var range = Enumerable.Range(1, 100);
-            foreach (int number in range)
+            var calculationService = new CalculationService();
+            var results = calculationService.Calculate(firstName, lastName);
+
+            foreach (string line in results)
             {
-                if (number % 3 == 0 && number % 5 == 0) Console.WriteLine($"{firstName} {lastName}");
-                else if (number % 3 == 0) Console.WriteLine(firstName);
-                else if (number % 5 == 0) Console.WriteLine(lastName);
-                else Console.WriteLine(number);
+                Console.WriteLine(line);
             }
         }
     }
