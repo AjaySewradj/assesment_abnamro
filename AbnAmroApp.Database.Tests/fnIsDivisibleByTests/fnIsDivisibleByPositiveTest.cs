a06669adaf7be361b65299b400ed9405b2df6ac9
Ajay Sewradj <ajaysewradj85@gmail.com>
2023-05-02 22:52:54 +0200

    Added database unit test project. Added first testcases

---
+++ AbnAmroApp.Database.Tests/fnIsDivisibleByTests/fnIsDivisibleByPositiveTest.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace AbnAmroApp.Database.Tests
+{
+    [TestClass()]
+    public class fnIsDivisibleByPositiveTest : SqlDatabaseTestClass
+    {
+
+        public fnIsDivisibleByPositiveTest()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction dbo_fnIsDivisibleByTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fnIsDivisibleByPositiveTest));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.ScalarValueCondition scalarValueCondition1;
+            this.dbo_fnIsDivisibleByTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            dbo_fnIsDivisibleByTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            scalarValueCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.ScalarValueCondition();
+            // 
+            // dbo_fnIsDivisibleByTest_TestAction
+            // 
+            dbo_fnIsDivisibleByTest_TestAction.Conditions.Add(scalarValueCondition1);
+            resources.ApplyResources(dbo_fnIsDivisibleByTest_TestAction, "dbo_fnIsDivisibleByTest_TestAction");
+            // 
+            // scalarValueCondition1
+            // 
+            scalarValueCondition1.ColumnNumber = 1;
+            scalarValueCondition1.Enabled = true;
+            scalarValueCondition1.ExpectedValue = "1";
+            scalarValueCondition1.Name = "scalarValueCondition1";
+            scalarValueCondition1.NullExpected = false;
+            scalarValueCondition1.ResultSet = 1;
+            scalarValueCondition1.RowNumber = 1;
+            // 
+            // dbo_fnIsDivisibleByTestData
+            // 
+            this.dbo_fnIsDivisibleByTestData.PosttestAction = null;
+            this.dbo_fnIsDivisibleByTestData.PretestAction = null;
+            this.dbo_fnIsDivisibleByTestData.TestAction = dbo_fnIsDivisibleByTest_TestAction;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod()]
+        public void dbo_fnIsDivisibleBy_GivenANumberDividableBy3_WhenTheDivisorIs3_ShouldReturnTrue()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_fnIsDivisibleByTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        [TestMethod()]
+        public void dbo_fnIsDivisibleByTest()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_fnIsDivisibleByTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+
+        private SqlDatabaseTestActions dbo_fnIsDivisibleByTestData;
+    }
+}
